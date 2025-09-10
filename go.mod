cc1257cac617e2ca8e316875cb95f2c725adb408
brunoTrinitario <brunotrinitario@gmail.com>
2025-09-09 23:58:30 -0300

    init: creacion de proyecto con go y smart contracts

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/BrunoTrinintario/rouletteGoBlockChain
+
+go 1.25.1
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Go Smart Contract Backend listo 🚀")
+}
